49e645c4bb686a4c168ede89e697491793dc3029
minhvuong43 <vuongchiminh9@gmail.com>
2024-04-23 21:07:20 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import Layout from "./Layout"
 import ProductList from "./ProductList"
-import Signup from "./Signup"
-import Signin from "./Signin"
 import ProductAdd from "./ProductAdd"
 import ProductEdit from "./ProductEdit"
 
@@ -15,8 +13,6 @@ function App() {
             <Route path="products" element={<ProductList />} />
             <Route path="products/add" element={<ProductAdd />} />
             <Route path="products/:id/edit" element={<ProductEdit />} />
-            <Route path="signup" element={<Signup />} />
-            <Route path="signin" element={<Signin />} />
         </Route>
       </Routes>
     </>
